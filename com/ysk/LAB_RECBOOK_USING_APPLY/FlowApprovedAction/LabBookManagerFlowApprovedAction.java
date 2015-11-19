d1c7156796ebf1558a08789ab17e6eb5d73217fe
B0050 <B0050@yungshingroup.com>
2015-11-19 10:13:59 +0800

    在紀錄簿管理人判斷紀錄簿編號輸入是否為空,此欄必填.

--- com/ysk/LAB_RECBOOK_USING_APPLY/FlowApprovedAction/LabBookManagerFlowApprovedAction.java
+++ com/ysk/LAB_RECBOOK_USING_APPLY/FlowApprovedAction/LabBookManagerFlowApprovedAction.java
@@ -1,4 +1,5 @@
 package com.ysk.LAB_RECBOOK_USING_APPLY.FlowApprovedAction;
+
 import SomeUtils.Bean.LabRecbookUsingApplyBean;
 import SomeUtils.DAO.LabRecbookUsingApplyDAO;
 //com/ysk/LAB_RECBOOK_USING_APPLY/FlowApprovedAction/LabBookManagerFlowApprovedAction;
@@ -6,20 +7,26 @@ import jcx.jform.bProcFlow;
 
 /**
  * 在紀錄簿管理人那一關,將會讓管理人update RECBOOK_NO欄位.
+ * 
  * @author YangTing
  *
  */
-public class LabBookManagerFlowApprovedAction extends bProcFlow{
+public class LabBookManagerFlowApprovedAction extends bProcFlow {
 
 	@Override
 	public boolean action(String arg0) throws Throwable {
 		// TODO Auto-generated method stub
-		
-		LabRecbookUsingApplyDAO lDao = new LabRecbookUsingApplyDAO(getTalk()); 
-		LabRecbookUsingApplyBean bean = lDao.getLabRecbookUsingApplyBeanByPno(getValue("PNO"));
+
+		if (getValue("RECBOOK_NO").trim().length() == 0) {
+			message("紀錄簿編號欄位不可為空.");
+			return false;
+		}
+		LabRecbookUsingApplyDAO lDao = new LabRecbookUsingApplyDAO(
+				getTalk());
+		LabRecbookUsingApplyBean bean = lDao
+				.getLabRecbookUsingApplyBeanByPno(getValue("PNO"));
 		bean.setRECBOOK_NO(getValue("RECBOOK_NO"));
 		lDao.update(bean);
-		
 		return true;
 	}
 
